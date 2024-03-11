76cc0594de78cc1573d86789f8192864701e5e09
h65rdl0ckl3z <bank427ingza@gmail.com>
2024-03-11 14:24:43 +0700

    first commit

--- src/main/java/org/bkl/bklcore/BKlCore.java
+++ src/main/java/org/bkl/bklcore/BKlCore.java
@@ -8,8 +8,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class BKlCore extends JavaPlugin {
 
     @Override
+    public void onLoad() {
+        getLogger().info("Load Plugin");
+    }
+
+    @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Enable Plugin");
 //        Command();
         GamePlay();
         GameRule();
