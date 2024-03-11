dab4fd1477076dabed43591f5bc9d434986eae1a
h65rdl0ckl3z <bank427ingza@gmail.com>
2024-03-11 16:55:02 +0700

    SleepZ

--- src/main/java/org/bkl/bklcore/gameplay/NoTreePunching.java
+++ src/main/java/org/bkl/bklcore/gameplay/NoTreePunching.java
@@ -1,16 +1,18 @@
 package org.bkl.bklcore.gameplay;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class NoTreePunching implements Listener {
     @EventHandler
-    public void onNoTreePunching(BlockBreakEvent blockBreakEvent) {
-        if (blockBreakEvent.getBlock().getType().equals(Material.OAK_LOG)) {
-            blockBreakEvent.setCancelled(true);
-
+    public void onNoTreePunching(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if (event.getBlock().getType().equals(Material.OAK_LOG)) {
+            event.setCancelled(true);
+            player.sendMessage("OAK-LOG Disable");
         }
     }
 }
