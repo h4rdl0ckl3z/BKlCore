2d35b1cb953e7daff125b7a49620a4c9c953839a
h65rdl0ckl3z <bank427ingza@gmail.com>
2024-03-11 18:00:56 +0700

    SleepZ

--- src/main/java/org/bkl/bklcore/gameplay/NoTreePunching.java
+++ src/main/java/org/bkl/bklcore/gameplay/NoTreePunching.java
@@ -10,9 +10,30 @@ public class NoTreePunching implements Listener {
     @EventHandler
     public void onNoTreePunching(BlockBreakEvent event) {
         Player player = event.getPlayer();
-        if (event.getBlock().getType().equals(Material.OAK_LOG)) {
-            event.setCancelled(true);
-            player.sendMessage("OAK-LOG Disable");
+//        if (event.getBlock().getType().equals(Material.OAK_LOG)) {
+//            event.setCancelled(true);
+//            player.sendMessage("OAK-LOG Disable");
+//        }
+//        if (player.getInventory().getItemInMainHand().getType().equals(Material.DIAMOND_AXE)) {
+//            event.setCancelled(false);
+//        }
+        Material[] log = {
+                Material.OAK_LOG,
+                Material.SPRUCE_LOG,
+                Material.BIRCH_LOG,
+                Material.JUNGLE_LOG,
+                Material.ACACIA_LOG,
+                Material.DARK_OAK_LOG,
+                Material.MANGROVE_LOG,
+                Material.CHERRY_LOG
+        };
+        for (int i = 0; i < log.length; i++) {
+            if (player.getInventory().getItemInMainHand().getType().equals(Material.DIAMOND_AXE)) {
+                event.setCancelled(false);
+            } else if (event.getBlock().getType().equals(log[i])) {
+                event.setCancelled(true);
+                player.sendMessage(String.valueOf(log[i]));
+            }
         }
     }
 }
