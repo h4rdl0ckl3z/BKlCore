71c2eab07a371360d010a3bcb6880fe4903c4698
h65rdl0ckl3z <bank427ingza@gmail.com>
2024-03-11 18:47:42 +0700

    SleepZ

--- src/main/java/org/bkl/bklcore/gameplay/NoTreePunching.java
+++ src/main/java/org/bkl/bklcore/gameplay/NoTreePunching.java
@@ -10,13 +10,12 @@ public class NoTreePunching implements Listener {
     @EventHandler
     public void onNoTreePunching(BlockBreakEvent event) {
         Player player = event.getPlayer();
-//        if (event.getBlock().getType().equals(Material.OAK_LOG)) {
-//            event.setCancelled(true);
-//            player.sendMessage("OAK-LOG Disable");
-//        }
-//        if (player.getInventory().getItemInMainHand().getType().equals(Material.DIAMOND_AXE)) {
-//            event.setCancelled(false);
-//        }
+        Material[] axe = {
+                Material.DIAMOND_AXE,
+                Material.IRON_AXE,
+                Material.STONE_AXE,
+                Material.NETHERITE_AXE
+        };
         Material[] log = {
                 Material.OAK_LOG,
                 Material.SPRUCE_LOG,
@@ -25,14 +24,32 @@ public class NoTreePunching implements Listener {
                 Material.ACACIA_LOG,
                 Material.DARK_OAK_LOG,
                 Material.MANGROVE_LOG,
-                Material.CHERRY_LOG
+                Material.CHERRY_LOG,
+                Material.STRIPPED_SPRUCE_LOG,
+                Material.STRIPPED_OAK_LOG,
+                Material.STRIPPED_JUNGLE_LOG,
+                Material.STRIPPED_DARK_OAK_LOG,
+                Material.STRIPPED_CHERRY_LOG,
+                Material.STRIPPED_BIRCH_LOG,
+                Material.STRIPPED_ACACIA_LOG,
+                Material.STRIPPED_MANGROVE_LOG
         };
+//        if (player.getInventory().getItemInMainHand().getType().equals(Material.DIAMOND_AXE)) {
+//            event.setCancelled(false);
+//        } else if (event.getBlock().getType().equals(Material.OAK_LOG)) {
+//            event.setCancelled(true);
+//            player.sendMessage(String.valueOf(Material.OAK_LOG));
+//        }
         for (int i = 0; i < log.length; i++) {
-            if (player.getInventory().getItemInMainHand().getType().equals(Material.DIAMOND_AXE)) {
-                event.setCancelled(false);
-            } else if (event.getBlock().getType().equals(log[i])) {
+            if (event.getBlock().getType().equals(log[i])) {
                 event.setCancelled(true);
                 player.sendMessage(String.valueOf(log[i]));
+            } else {
+                for (int j = 0; j < axe.length; j++) {
+                    if (player.getInventory().getItemInMainHand().getType().equals(axe[j])) {
+                        event.setCancelled(false);
+                    }
+                }
             }
         }
     }
